5577de69a8245d58af47a7ff714f3f379f1c389c
Shuo Lv <claus.lv@hotmail.com>
2014-01-23 09:00:42 +0800

    Edit items

---
+++ 05 - Edit Items/Checklists/ChecklistsViewController.h
@@ -0,0 +1,14 @@
+//
+//  ChecklistsViewController.h
+//  Checklists
+//
+//  Created by Matthijs on 30-09-13.
+//  Copyright (c) 2013 Razeware LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ItemDetailViewController.h"
+
+@interface ChecklistsViewController : UITableViewController <ItemDetailViewControllerDelegate>
+
+@end
