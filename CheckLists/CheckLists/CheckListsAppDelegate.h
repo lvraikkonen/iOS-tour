bc8bab1a6e6139936327eca30ec572a8cb50a227
Shuo Lv <claus.lv@hotmail.com>
2014-03-20 18:24:17 +0800

    Initial Build

---
+++ CheckLists/CheckLists/CheckListsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CheckListsAppDelegate.h
+//  CheckLists
+//
+//  Created by Claus on 14-3-20.
+//  Copyright (c) 2014年 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CheckListsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CheckLists/CheckLists/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CheckLists
+//
+//  Created by Claus on 14-3-20.
+//  Copyright (c) 2014年 self.edu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CheckListsAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CheckListsAppDelegate class]));
+    }
+}
